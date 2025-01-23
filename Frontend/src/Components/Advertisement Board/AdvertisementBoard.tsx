63cb3db6d2c87b8e8c660353969bec6f680966f1
Shanks-1203 <ravichandran.shankar@divum.in>
2025-01-23 11:24:45 +0530

    revisit commit

--- Frontend/src/Components/Advertisement Board/AdvertisementBoard.tsx
+++ Frontend/src/Components/Advertisement Board/AdvertisementBoard.tsx
@@ -1,17 +1,15 @@
-// import weeknd from '../../Assets/Images/The Weeknd.png'
-// import logo from '../../Assets/Images/Logo.png'
-import anirudh from '../../Assets/Images/Anirudh.png'
+import weeknd from '../../Assets/Images/The Weeknd.png';
 import { BsDot } from "react-icons/bs";
 
 const AdvertisementBoard = () => {
   return (
-    <div className='h-[18rem] flex rounded-lg items-center p-[2rem] justify-between text-white bg-gradient-to-r cursor-pointer to-red-400 from-red-500'>
+    <div className='h-[18rem] flex rounded-lg items-center pl-[2rem] justify-between text-white bg-gradient-to-r cursor-pointer to-purple-400 from-purple-500'>
         <div className='flex text-md w-[65%] flex-col justify-center'>
-            <p className='text-2xl font-semibold'>Anirudh Hits</p>
+            <p className='text-2xl font-semibold'>Weeknd Hits</p>
             <p className='flex items-center'>2 Tracks <BsDot/> Created by Shanks</p>
-            <p className='opacity-65 w-[60%] mt-6'>Listen to the trending songs of Anirudh Ravichander now.</p>
+            <p className='opacity-65 w-[60%] mt-6'>Listen to the trending songs of The Weeknd now.</p>
         </div>
-        <img src={anirudh} alt='the weeknd' className='h-full rounded-lg'/>
+        <img src={weeknd} alt='the weeknd' className='h-full rounded-lg'/>
     </div>
   )
 }
